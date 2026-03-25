191cb00baf2b08098d3a766aafa9b011d4cfeb8c
Alexander Perlin <alexander.perlin@intel.com>
2026-03-25 12:27:23 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
+  }
+}
+
+# Use local kubeconfig (Minikube)
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
